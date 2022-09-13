a285ac9ed275e2a1796750ec2d02298684d1b5c3
ashley31836 <hiashleyyu@gmail.com>
2022-09-12 23:31:36 -0700

    Made changes to index.js and generateMarkdown.js

--- index.js
+++ index.js
@@ -1,13 +1,69 @@
 // TODO: Include packages needed for this application
-
+const inquirer = require('inquirer');
+const fs = require('fs');
+const generateMarkdown = require('./utils/generateMarkdown');
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+  {
+    type: 'list',
+    name: 'license',
+    message: 'Choose a license for your project:',
+    choices: ['Apache 2.0', 'MIT','Mozilla 2.0', 'None']
+  },
+  {
+    type: 'input',
+    name: 'title',
+    message: 'What is your project title?',
+  },
+  {
+    type: 'input',
+    name: 'description',
+    message: 'What does this project do?'
+  },
+  {
+    type: 'input',
+    name: 'installation',
+    message: 'How do I install this project?'
+  },
+  {
+    type: 'input',
+    name: 'usage',
+    message: 'How do I use this project?'
+  },
+  {
+    type: 'input',
+    name: 'contribution',
+    message: 'How can others contribute to this project?'
+  },
+  {
+    type: 'input',
+    name: 'test',
+    message: 'How do you test this project?'
+  },
+  {
+    type: 'input',
+    name: 'github',
+    message: 'What is your GitHub username?'
+  },
+  {
+    type: 'input',
+    name: 'email',
+    message: 'What is your email?'
+  }
+];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+  fs.writeFile(fileName, data, (err) =>
+  err ? console.error(err) : console.log('README has been generated.'))
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+  inquirer.prompt(questions).then((data) => {
+    writeToFile('README.md', generateMarkdown(data))
+  })
+}
 
 // Function call to initialize app
 init();
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,10 +1,30 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  if (license === 'Apache 2.0') {
+    return `![License](https://img.shields.io/badge/License-Apache%202.0-brightgreen)`;
+  } else if (license === 'MIT') {
+    return `![License](https://img.shields.io/badge/License-MIT-yellow)`;
+  } else if (license === 'Mozilla 2.0') {
+    return `![License](https://img.shields.io/badge/License-Mozilla%202.0-red)`;
+  } else {
+    return '';
+  }
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  if (license === 'Apache 2.0') {
+    return 'https://opensource.org/licenses/Apache-2.0';
+  } else if (license === 'MIT') {
+    return 'https://opensource.org/licenses/MIT';
+  } else if (license === 'Mozilla 2.0') {
+    return 'https://opensource.org/licenses/MPL-2.0';
+  } else {
+    return '';
+  }
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
